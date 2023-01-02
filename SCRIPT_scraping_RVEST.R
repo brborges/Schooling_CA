0b00efab983bb4f87b9b3fe698c0514694803f61
Bruno Borges <brunoborges2004@yahoo.com.br>
2023-01-01 22:22:43 -0300

    Scraping using RVEST - Working v01

---
+++ SCRIPT_scraping_RVEST.R
@@ -0,0 +1,132 @@
+# Packages -----------------------------------------------------------------
+library(rvest)
+library(tidyverse)
+
+
+# Importing --------------------------------------------------------------
+
+
+## Importing table os schools ##
+
+## main link
+link <- "https://www.city-data.com/schools-dirs/schools-CA.html"
+
+## creating a dataframe
+schools <- data.frame()
+
+## looping to import the schools in all the 129 pages
+
+for (page_result in seq(from = 0, to = 129, by = 1)) {
+
+  link <- paste0("https://www.city-data.com/schools-dirs/schools-CA", page_result,".html")
+  page <- rvest::read_html(link)
+  page_table <- page %>% html_nodes("table") %>% html_table() %>% .[[8]]
+  schools <- rbind(schools, page_table)
+  print(paste("Page:", page_result))
+  
+}
+
+## creating list of schools links
+link_school <- schools %>%
+                  select(Name) %>% 
+                  mutate(link = stringr::str_to_lower(Name)) %>% 
+                  mutate(link = stringr::str_replace_all(link," ","-")) %>% 
+                  mutate(link = stringr::str_replace_all(link,"/","-")) %>% 
+                  mutate(link = stringr::str_replace_all(link,"[[:punct:]]", "-")) %>% 
+                  mutate(link = stringr::str_replace_all(link,"---","-")) %>%
+                  mutate(link = stringr::str_replace_all(link,"--","-")) %>%
+                  mutate(link_school = paste0("https://www.city-data.com/school/",link,"-ca.html")) %>% 
+                  select(link_school)
+
+
+## Importing details info from each school
+
+details_list <- data.frame()
+df_details <- data.frame()
+
+get_details <- function(link_school){
+  
+  #link_school <- "https://www.city-data.com/school/zela-davis-school-ca.html"
+  #link_school <-"https://www.city-data.com/school/a-b-c-little-school-ca.html" 
+  page_school <- rvest::read_html(link_school)
+  length_details <- length(page_school %>% rvest::html_nodes("dd"))
+  
+  address <- page_school %>% rvest::html_nodes(".addr") %>% rvest::html_text() 
+  city <- page_school %>% rvest::html_nodes("a") %>% rvest::html_text() %>% .[[322]]
+  
+  ## testing the length first
+  
+  if (length_details >=2) {
+    telephone <- page_school %>% rvest::html_nodes("dd") %>%  rvest::html_text() %>% .[[2]]
+  }else {
+    telephone <- ""
+  }
+  
+  if (length_details >=4) {
+    students <- page_school %>% rvest::html_nodes("dd") %>% rvest::html_text() %>% .[[4]]
+  }else {
+    students <- ""
+  }
+  
+  if (length_details >=5) {
+    classroom_teachers <- page_school %>% rvest::html_nodes("dd")%>%  rvest::html_text() %>% .[[5]]
+  }else {
+    classroom_teachers <- ""
+  }
+ 
+  if (length_details >=7) {
+    free_lunch_students <- page_school %>% rvest::html_nodes("dd") %>% rvest::html_text() %>% .[[7]]
+  }else {
+    free_lunch_students <- ""
+  }
+  
+  if (length_details >=8) {
+    reduce_price_lunch <- page_school %>% rvest::html_nodes("dd")%>%  rvest::html_text() %>% .[[8]]
+  }else {
+    reduce_price_lunch <- ""
+  }
+  
+  school_district <- page_school %>% rvest::html_nodes("#sdName")%>% rvest::html_text()
+    if (length(school_district != 0)) {
+      school_district <- page_school %>% rvest::html_nodes("#sdName")%>% rvest::html_text()
+    }else {
+      school_district <- ""
+    }
+  
+  school_name <- page_school %>% rvest::html_nodes("b")%>% rvest::html_text() %>% .[[10]]
+
+  df_details <- tibble::tibble(school_name,address,city,school_district,telephone,students,classroom_teachers,free_lunch_students,reduce_price_lunch)
+  
+  details_list <- rbind(details_list, df_details)
+  
+  return (details_list)
+}
+
+## initiating variables
+
+x <- 1
+df_schools_details <- data.frame()
+temp <- data.frame()
+
+## looping to create the school details dataframe
+
+for (x in 1:length(unlist(link_school))) {
+  
+  temp <- get_details(link_school[[x,1]])
+  df_schools_details <- rbind(df_schools_details, temp)
+  print(paste("Capturing details:", x))
+
+}
+df_schools_details
+
+
+# Tidy -------------------------------------------------------------
+
+
+
+# Visualization ------------------------------------------------------------
+
+
+# Exporting --------------------------------------------------------------
+
+
